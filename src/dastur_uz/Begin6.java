17357e6887042d9af6cdee1d5fb1442459cd5df6
Mukhammadali Makhmudov <mukhammadalimakhmudov@Mukhammadalis-MacBook-Pro.local>
2024-01-03 18:15:52 +0100

    new

--- src/dastur_uz/Begin6.java
+++ src/dastur_uz/Begin6.java
@@ -2,28 +2,30 @@ package dastur_uz;
 
 import java.util.Scanner;
 
-public class Begin3 {
+public class Begin6 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("To'rtburchak tomonlari uchun a sonni kiriting: ");
+        System.out.print("Paralelepepidning tomonlari uchun a sonni kiriting: ");
         int aNumber = sc.nextInt();
-        System.out.print("To'rtburchak tomonlari uchun b sonni kiriting: ");
+        System.out.print("Paralelepepidning tomonlari uchun b sonni kiriting: ");
         int bNumber = sc.nextInt();
+        System.out.print("Paralelepepidning tomonlari uchun c sonni kiriting: ");
+        int cNumber = sc.nextInt();
 
-        System.out.println("Berilgan sonning yuzasi = " + yuzaCal(aNumber,bNumber));
-        System.out.println("Berilgan sonning perimetri = " + perimetrCal(aNumber,bNumber));
+        System.out.println("Berilgan sonning hajmi = " + hajmiCal(aNumber,bNumber,cNumber));
+        System.out.println("Berilgan sonning sirti = " + sirtiCal(aNumber,bNumber,cNumber));
 
     }
 
-    static int yuzaCal(int a, int b){
-        int S = a * b;
+    static int hajmiCal(int a, int b,int c){
+        int V = a * b * c;
+        return V;
+    }
+
+    static int sirtiCal(int a, int b,int c){
+        int S = 2 * (a * b + b * c + a * c);
         return S;
-    }
-
-    static int perimetrCal(int a, int b){
-        int P = 2 * (a + b);
-        return P;
     }
 }
